532ae1fc59ed113078846d30315c7bf2a442f076
Hexagons <anton@hexagons.se>
2019-09-25 18:03:14 +0200

    Initial Commit

---
+++ PixelTransitions/PixelTransitions.h
@@ -0,0 +1,19 @@
+//
+//  PixelTransitions.h
+//  PixelTransitions
+//
+//  Created by Hexagons on 2019-09-25.
+//  Copyright © 2019 Hexagons. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PixelTransitions.
+FOUNDATION_EXPORT double PixelTransitionsVersionNumber;
+
+//! Project version string for PixelTransitions.
+FOUNDATION_EXPORT const unsigned char PixelTransitionsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PixelTransitions/PublicHeader.h>
+
+
